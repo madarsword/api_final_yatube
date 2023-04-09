e7d11efbf3f30ece9a019b33aba3de9ad3718294
al-shp <alshp@tutanota.com>
2023-04-09 20:28:36 +0200

    final2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.generics import get_object_or_404
 from rest_framework.validators import UniqueTogetherValidator
 
 from posts.models import Comment, Follow, Group, Post, User
